c90d85b7154ead7c532159577056029ead2b3942
WanglinSong <wsong14@hawk.iit.edu>
2019-01-17 17:46:07 -0600

    clean up, waiting for restructuring and deploy

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -70,7 +70,7 @@ class RequestResetForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
+            raise ValidationError('There is no account associated with that email. You must register first.')
 
 
 class ResetPasswordForm(FlaskForm):
